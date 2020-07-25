b67c284946a15f6f9d4d67e3fd668bf1e066cb49
Hetoreno <hectortamriel@gmail.com>
2020-07-24 21:03:21 -0700

    updating

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let heightValue = document.querySelector(".height");
 let weightValue = document.querySelector(".weight");
 const calculate = document.querySelector(".submit");
+const display = document.querySelector(".value"); 
 
 calculate.addEventListener("click" , function(){
     let realHeight = heightValue.value;
     let realWeight = weightValue.value;
     heightValue.value = '';
     weightValue.value = '';
-
-    let totalBMI = realWeight / (realHeight * realHeight);
+    var totalBMI = realWeight / (realHeight * realHeight);
     console.log(totalBMI);
+
+    display.innerText = totalBMI;
 });
 
